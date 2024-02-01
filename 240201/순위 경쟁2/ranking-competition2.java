750e500cf89bb51dd8e690b8db396347001ca953
Soap <87180146+Miensoap@users.noreply.github.com>
2024-02-01 15:45:22 +0900

    Title: 순위 경쟁2, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240201/순위 경쟁2/ranking-competition2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    static String glory = "";
+    static int[] score = new int[2];
+    static int cnt = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            setScore(sc.nextLine().split(" "));
+            setGlory();
+        }
+        System.out.println(cnt);
+    }
+
+    static void setScore(String[] in) {
+        if (in[0].equals("A")) score[0] = score[0] + Integer.parseInt(in[1]);
+        else score[1] = score[1] + Integer.parseInt(in[1]);
+    }
+
+    static void setGlory() {
+        StringBuilder sb = new StringBuilder();
+        int maxScore = 0;
+        for (int j : score) {
+            maxScore = Math.max(j, maxScore);
+        }
+        for (int i = 0; i < score.length; i++) {
+            if (score[i] == maxScore) sb.append(i);
+        }
+
+        if (!glory.equals(sb.toString())) {
+            glory = sb.toString();
+            cnt++;
+        }
+    }
+
+
+}
