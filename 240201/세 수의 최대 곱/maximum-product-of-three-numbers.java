088290c8b8cbd04d5d6c4b618529fa4b3bcc38fb
Soap <87180146+Miensoap@users.noreply.github.com>
2024-02-01 16:25:27 +0900

    Title: 세 수의 최대 곱, Time: 263ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240201/세 수의 최대 곱/maximum-product-of-three-numbers.java
+++ 240201/세 수의 최대 곱/maximum-product-of-three-numbers.java
@@ -5,6 +5,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
         int a;
+        int result1 =0;
+        int result2 =0;
         List<Integer> plus = new ArrayList<>();
         List<Integer> minus = new ArrayList<>();
         for(int i=0; i<n; i++){
@@ -16,10 +18,21 @@ public class Main {
         minus.sort(Integer::compareTo);
         Collections.reverse(plus);
 
+        if(plus.size()==0) {
+            if(minus.contains(0)) {
+                System.out.println(0);
+                return;
+            }
+            System.out.println(minus.get(minus.size()-1)*minus.get(minus.size()-2)*minus.get(minus.size()-3));
+            return;
+        }
+
         //case 1 ppp
-        int result1 = plus.get(0)*plus.get(1)*plus.get(2);
+        if(plus.size()>=3) result1 = plus.get(0)*plus.get(1)*plus.get(2);
         // case 2 pmm
-        int result2 = plus.get(0)*minus.get(0)*minus.get(1);
+        if(minus.size()>=2) result2 = plus.get(0)*minus.get(0)*minus.get(1);
+
+
 
         System.out.println(Math.max(result1,result2));
     }
