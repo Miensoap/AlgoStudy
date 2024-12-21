f69e7108cdc13c93766c4e02ada904386529cd5b
miensoap <miensop51@gmail.com>
2024-12-21 10:15:30 +0900

    241221
    문자열

---
+++ Inflearn/P0102/Main.java
@@ -0,0 +1,20 @@
+package Inflearn.P0102;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String inputString = in.nextLine();
+        char[] chars = inputString.toCharArray();
+
+        StringBuilder result = new StringBuilder();
+        for (char c : chars) {
+            if (Character.isUpperCase(c)) result.append(Character.toLowerCase(c));
+            else result.append(Character.toUpperCase(c));
+        }
+
+        System.out.println(result);
+    }
+}
---
+++ Inflearn/P0103/Main.java
@@ -0,0 +1,17 @@
+package Inflearn.P0103;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] strings = in.nextLine().split(" ");
+
+        String result = strings[0];
+        for (String string : strings) {
+            if (result.length() < string.length()) result = string;
+        }
+
+        System.out.println(result);
+    }
+}
